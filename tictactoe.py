faa0f9e2db9eacfd824bd0527e75862582b8d816
Daniel Wang <dalbertw@unc.edu>
2024-08-01 21:24:16 -0400

    creation of tic tac toe project

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+
+#Global Variables
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# Printing the game board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
+
+# Taking a player input
+
+# Check for a win or tie
+
+# Switch the player
+
+# Check for win or tie again
